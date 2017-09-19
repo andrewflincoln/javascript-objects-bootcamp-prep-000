2e6d487c45910d7b4d3417a514a338f315ff143c
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-19 20:03:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(list, newArtist, newSong) {
   list[newArtist] = newSong
   return list
 }
+
+function removeFromPlaylist(list, artist) {
+  delete list[artist]
+  return list
+}
